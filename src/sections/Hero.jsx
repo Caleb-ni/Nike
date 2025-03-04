b3ebd72cf0788cb3f81021dc4a46e4eb504c18bd
Caleb Niyomukiza <niyocaleb01@gmail.com>
2025-03-05 00:08:37 +0200

    Initial commit

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,9 +1,13 @@
-import Button from "../../Button"
 import { arrowRight } from "../assets/icons"
-import { statistics } from "../constants"
+import { shoes, statistics } from "../constants"
 import { bigShoe1 } from "../assets/images"
+import Button from "../Components/Button"
+import ShoeCard from "../Components/ShoeCard"
+import { useState } from "react"
+
 
 const Hero = () => {
+    const [bigShoeImg, setBigShoeImg] = useState(bigShoe1); 
   return (
     <section
       id="home"
@@ -11,7 +15,7 @@ const Hero = () => {
     >
       <div className="relative xl:w-2/5 flex-col justify-center items-start w-full max-xl:padding-x pt-28">
         <p className="text-xl font-montserrat text-coral-red">Our Summer Collection</p>
-        <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max=-sm:leading-[82] font-bold">
+        <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold">
           <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">The New Arrival</span>
           <br />
           <span className="text-coral-red inline-block mr-3">Nike</span>
@@ -24,22 +28,36 @@ const Hero = () => {
           {statistics.map((stat) => (
             <div key={stat.label}>
               <p className="text-4xl font-palanquin font-bold">{stat.value}</p>
-              <p className="leading-7 font-montserrat text-slate-bold ">{stat.label}</p> 
+              <p className="leading-7 font-montserrat text-slate-gray">
+                {stat.label}
+              </p> 
             </div>
           ))} 
         </div>
       </div>
 
-      <div className="relative flex-1 flex justify-center items-center xm:min-h-screem max-xl:py -40 bg-primary bg-hero">
+      <div className="relative flex-1 flex justify-center items-center xm:min-h-screem max-xl:py-40 bg-primary bg-hero bg-cover bg-center">
         <img 
-        src={bigShoe1} 
+        src={bigShoeImg} 
         alt="shoe collection" 
         width={610}
         height={500}
         className="object-contain relative z-10"
         />
+        <div className='flex sm:gap-6 gap-4 absolute -bottom-[5%] sm:left-[10%] max-sm:px-6'>
+          {shoes.map((shoe) => (
+            <div key={shoe.thumbnail}>
+              <ShoeCard
+                imgURL={shoe}
+                changeBigShoeImage={(shoe) =>  setBigShoeImg(shoe)}
+                bigShoeImg= {bigShoeImg}  
+              />
+            </div>
+          ))}
+        </div>
       </div>
-    </section>
+      
+    </section> 
   )
 }
 
